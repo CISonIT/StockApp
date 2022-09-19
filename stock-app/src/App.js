7c5cdfb8894eb9f5c93bab2590764e863d01dd19
CISonIT <jakubcisonIT@gmail.com>
2022-09-19 19:58:24 +0200

    working with chart+stock apis

--- stock-app/src/App.js
+++ stock-app/src/App.js
@@ -1,21 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { StockDetailPage } from "./pages/StackDetailPage";
 import { StockOverviewPage } from "./pages/StockOverviewPage";
 import { NotFound } from "./pages/NotFound";
+import { WatchListContextProvider } from "./context/watchListContext"
 
 function App() {
   return (
     <main className="container">
-      <BrowserRouter>
-        <Routes>
-          <Route  path='/'
-                  element={<StockOverviewPage/>}/>
-          <Route  path='/detail/:symbol' 
-                  element={<StockDetailPage/>}/>
-          <Route  path='*'
-                  element={<NotFound/>}/>
-        </Routes>
-      </BrowserRouter>
+      <WatchListContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route  path='/'
+                    element={<StockOverviewPage/>}/>
+            <Route  path='/detail/:symbol' 
+                    element={<StockDetailPage/>}/>
+            <Route  path='*'
+                    element={<NotFound/>}/>
+          </Routes>
+        </BrowserRouter>
+      </WatchListContextProvider>
     </main>
   );
 }
--- stock-app/src/components/StockList.jsx
+++ stock-app/src/components/StockList.jsx
@@ -1,10 +1,26 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext } from "react"
 import finnHub from "../api/finnHub"
+import { BsFillCaretDownFill, BsFillCaretUpFill } from "react-icons/bs"
+import { WatchListContext } from '../context/watchListContext'
+import { useNavigate } from "react-router-dom"
 
 export const StockList = () => {
 
-    const [stock, setStock] = useState();
-    const [watchList, setWatchList] = useState(["GOOGL", "AMZN", "MSFT"])
+    const [stock, setStock] = useState([]);
+    const { watchList } = useContext(WatchListContext);
+    const navigate = useNavigate();
+
+    const changeColor = (change) => {
+        return change > 0 ? "success" : "danger";
+    }
+
+    const renderIcon = (change) => {
+        return change > 0 ? <BsFillCaretUpFill/> : <BsFillCaretDownFill/>;
+    }
+
+    const handleStockSelect = (symbol) => {
+        navigate(`detail/${symbol}`)
+    }
 
     useEffect(() => {
         let isMounted = true;
@@ -37,9 +53,40 @@ export const StockList = () => {
         fetchData();
 
         return () => (isMounted = false)
-    }, [])
+    }, [watchList])
 
     return (
-        <div>SIEMA</div>
+        <div>
+            <table className="table hover mt-5">
+                <thead style={ {color: "rgb*79,89,102)"}}>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Chg</th>
+                        <th scope="col">Chg%</th>
+                        <th scope="col">High</th>
+                        <th scope="col">Low</th>
+                        <th scope="col">Open</th>
+                        <th scope="col">Pclose</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {stock.map((stockData) => {
+                        return (
+                            <tr style={{cursor: "pointer"}} onClick={() => handleStockSelect(stockData.symbol)} className="table-row" key={stockData.symbol}>
+                                <th scope="row">{stockData.symbol}</th>
+                                <td>{stockData.data.c}</td>
+                                <td className={`text-${changeColor(stockData.data.d)}`}>{stockData.data.d} {renderIcon(stockData.data.d)} </td>
+                                <td className={`text-${changeColor(stockData.data.d)}`}>{stockData.data.dp} {renderIcon(stockData.data.d)} </td>
+                                <td>{stockData.data.h}</td>
+                                <td>{stockData.data.l}</td>
+                                <td>{stockData.data.o}</td>
+                                <td>{stockData.data.pc}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
     )
 }
